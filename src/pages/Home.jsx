e6c028db987c370144b3d17483ca215517a4c6db
MarinoBarada <marino18barada@gmail.com>
2023-07-04 17:28:46 +0200

    Kreiranje komponente Banner i kreiranje komponente FirstSlider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Header from "../components/Header";
 import Categories from "../components/Categories";
+import Banner from "../components/Banner";
+import FirstSlider from "../components/firstSlider";
 
 function Home() {
   return (
     <>
       <Header />
       <Categories />
+      <div className="wrapper flex-column">
+        <Banner />
+        <FirstSlider />
+      </div>
     </>
   );
 }
